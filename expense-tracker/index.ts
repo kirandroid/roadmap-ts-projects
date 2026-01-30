bfae15b90fb3ab3685372736e637078025cb1b16
kirandroid <pradhanangakiran@gmail.com>
2026-01-30 17:03:11 +0900

    add remaining commander commands, complete expense tracker

--- expense-tracker/index.ts
+++ expense-tracker/index.ts
@@ -9,6 +9,27 @@ program
   .description('CLI to track your expenses')
   .version('1.0.0');
 
+
+program.command('add')
+  .description('Add a new expense')
+  .option('-d, --description <string>', 'Description of the expense')
+  .option('-a, --amount <number>', 'Amount of the expense')
+  .action((options) => {
+    const description = options.description;
+    const amount = options.amount ? parseInt(options.amount) : undefined;
+    if (description == undefined || amount == undefined) {
+      console.log('Description and amount are required');
+      process.exit(1);
+    }
+    controller.addExpense(description, amount);
+  });
+
+program.command('list')
+  .description('List all expenses')
+  .action(() => {
+    controller.listExpenses();
+  });
+
 program.command('summary')
   .description('Display a summary of expenses')
   .option('-m, --month <number>', 'Month as a number (1-12)')
@@ -17,6 +38,18 @@ program.command('summary')
     const month = options.month ? parseInt(options.month) : undefined;
     const year = options.year ? parseInt(options.year) : undefined;
     controller.summaryOfExpenses({ month, year });
+  });
+
+program.command('delete')
+  .description('Delete an expense')
+  .option('-i, --id <number>', 'ID of the expense')
+  .action((options) => {
+    const id = options.id ? parseInt(options.id) : undefined;
+    if (id == undefined) {
+      console.log('ID is required');
+      process.exit(1);
+    }
+    controller.deleteExpense(id);
   });
 
 program.parse();
