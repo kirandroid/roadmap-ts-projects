adfe8dd13e64f3f1f437b955b0f129cfa7027ef6
kirandroid <pradhanangakiran@gmail.com>
2026-02-05 10:04:42 +0900

    unit converter init

--- git-user-activity/src/app.ts
+++
@@ -1,8 +0,0 @@
-async function fetchData() {
-    const response = await fetch("https://api.github.com/users/jaygaha/events");
-    const data = await response.json();
-    console.log(data);
-
-}
-
-fetchData();
